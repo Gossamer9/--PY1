dc5e98e56fce4501017715f76e8fdacba56541f7
Gossamer9 <115527230+Gossamer9@users.noreply.github.com>
2022-10-17 06:45:32 +0300

    Update task9
    Fixed

--- Лабораторная работа 3/task9.py
+++ Лабораторная работа 3/task9.py
@@ -3,9 +3,7 @@ spend = 6000  # траты
 months = 10  # количество месяцев
 increase = 0.03  # рост цен
 need_money = 0
-need_money = need_money + spend - salary
-for i in range(1,months):
-    spend*=1.03
+for i in range(months):
     need_money = need_money + spend - salary
-
+    spend*=(1+increase)
 print(round(need_money))
